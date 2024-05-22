faae8c7e66cfb2198d30688b8c04a68622da7b53
fajarmori <mrfajar.official@gmail.com>
2024-05-22 16:00:48 +0700

    add menu Sites

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,8 +12,13 @@
                 @auth
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('sites.index')" :active="request()->routeIs('sites.*')">
+                        {{ __('Sites') }}
                     </x-nav-link>
                 </div>
                 @endauth
@@ -75,8 +80,13 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         @auth
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')">
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('sites.index')" :active="request()->routeIs('sites.*')">
+                {{ __('Sites') }}
             </x-responsive-nav-link>
         </div>
         @endauth
