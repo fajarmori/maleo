42546d3bb7d3c1618b0bd20f55f51f90c9043fe7
fajarmori <mrfajar.official@gmail.com>
2024-05-22 15:47:41 +0700

    add route resource for sites

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [Controllers\ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [Controllers\ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/sites', Controllers\SiteController::class);
 });
 
 require __DIR__.'/auth.php';
