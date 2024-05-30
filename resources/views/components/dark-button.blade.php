171d8478c460351b94392ca03768c0c8d2aa0465
fajarmori <mrfajar.official@gmail.com>
2024-05-30 11:12:58 +0700

    logical button or anchor for button

---
+++ resources/views/components/dark-button.blade.php
@@ -0,0 +1,14 @@
+@props([
+    'type' => 'submit',
+    'as' => 'button',
+])
+
+@if ($as === 'button')
+<button type="{{ $type }}" {{ $attributes->merge(['class' => 'text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700']) }} >
+    {{ $slot }}
+</button>
+@else
+<a {{ $attributes->merge(['class' => 'inline-block text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700']) }}>
+    {{ $slot }}
+</a>
+@endif
--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,3 +1,14 @@
-<button {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+@props([
+    'type' => 'submit',
+    'as' => 'button',
+])
+
+@if ($as === 'button')
+<button type="{{ $type }}" {{ $attributes->merge(['class' => 'text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800']) }} >
     {{ $slot }}
 </button>
+@else
+<a {{ $attributes->merge(['class' => 'inline-block text-white text-center bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800']) }}>
+    {{ $slot }}
+</a>
+@endif
