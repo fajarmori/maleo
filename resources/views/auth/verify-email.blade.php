480c2dc8215fd614329e5afb190a2d50e3c4ef68
fajarmori <mrfajar.official@gmail.com>
2024-06-06 11:51:40 +0700

    custom display verify email

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,31 +1,38 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-    </div>
+<x-app-layout>
+    @slot('title','Verify Email')
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Verify Email') }} <span class="text-grey-500 font-normal italic">({{auth()->user()->email}})</span>
+        </h2>
+    </x-slot>
 
-    @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
-        </div>
-    @endif
+    <x-container class="">
+        <div class="flex justify-center">
+            <div class="w-full sm:max-w-md px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg text-gray-900">
+                <div class="mb-4 text-sm text-gray-600 text-justify">
+                    {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+                </div>
 
-    <div class="mt-4 flex items-center justify-between">
-        <form method="POST" action="{{ route('verification.send') }}">
-            @csrf
+                @if (session('status') == 'verification-link-sent')
+                    <div class="mb-4 font-medium text-sm text-green-600">
+                        {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                    </div>
+                @endif
 
-            <div>
-                <x-primary-button>
-                    {{ __('Resend Verification Email') }}
-                </x-primary-button>
+                <div class="mt-4 flex items-center justify-between">
+                    <form method="POST" action="{{ route('verification.send') }}">
+                        @csrf
+
+                        <div>
+                            <x-primary-button>
+                                {{ __('Resend Verification Email') }}
+                            </x-primary-button>
+                        </div>
+                    </form>
+
+                    <a href="{{ route('profile.edit') }}" class="underline text-gray-600">Edit Profile</a>
+                </div>
             </div>
-        </form>
-
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-
-            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                {{ __('Log Out') }}
-            </button>
-        </form>
-    </div>
-</x-guest-layout>
+        </div>
+    </x-container>
+</x-app-layout>
