0503f8e639ffdc910d6a2e02c49c7c877160367a
fajarmori <mrfajar.official@gmail.com>
2024-06-12 10:09:46 +0700

    added user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,6 +15,10 @@ class UserSeeder extends Seeder
             ['name' => 'Maleo Rachma', 'email' => 'admin@mria.co.id', 'type' => 2, 'password' => 'password'],
             ['name' => 'Indo Abadi', 'email' => 'info@mria.co.id', 'type' => 1, 'password' => 'password'],
             ['name' => 'Mohamad Rizki Fajar', 'email' => 'fajar@mria.co.id', 'type' => 0, 'password' => 'password'],
+            ['name' => 'Raihan Farid Rachmaditya', 'email' => 'raihan.farid@mria.co.id', 'type' => 2, 'password' => 'password'],
+            ['name' => 'Adella Putri Grimuarti', 'email' => 'adella.putri@mria.co.id', 'type' => 2, 'password' => 'password'],
+            ['name' => 'Alfarri Febriana', 'email' => 'alfarri@mria.co.id', 'type' => 2, 'password' => 'password'],
+            ['name' => 'Agung Wicaksono', 'email' => 'agung.wicaksono@mria.co.id', 'type' => 2, 'password' => 'password'],
         ]);
 
         $users->each(function($user){
