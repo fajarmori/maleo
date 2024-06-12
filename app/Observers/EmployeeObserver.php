fe5ec064fb326171a3eba2a782270347613b6c04
fajarmori <mrfajar.official@gmail.com>
2024-06-12 10:09:13 +0700

    make rule name headline, reconfig mria, rule phone, and rule slug employee

--- app/Observers/EmployeeObserver.php
+++ app/Observers/EmployeeObserver.php
@@ -8,8 +8,19 @@ class EmployeeObserver
 {
     public function creating(Employee $employee): void 
     {
+        $employee->name = str()->headline($employee->name);
+
         $data = Employee::latest('id')->first();
-        $data ? $mria = $employee->mria : $mria = 0;
+        $data ? $mria = $data->mria : $mria = 0;
         $employee->mria = $mria+1;
+
+        substr($employee->phone,0,1) == 0 ? $phone = '62'.substr($employee->phone,1) : $phone = $employee->phone;
+        $employee->phone = $phone;
+
+        do{
+            $slug = str()->random(40);
+            $check = Employee::where('slug', $slug)->first();
+        } while (isset($check));
+        $employee->slug = $slug;
     }
 }
