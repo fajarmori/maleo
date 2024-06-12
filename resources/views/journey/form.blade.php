d5225ed87894e4cb8c58a5592d899560f4603f5f
fajarmori <mrfajar.official@gmail.com>
2024-06-12 10:02:35 +0700

    added input and change value button to Save

--- resources/views/journey/form.blade.php
+++ resources/views/journey/form.blade.php
@@ -13,26 +13,44 @@
                     @method($page_meta['method'])
                     @csrf
                     <div>
-                        <x-input-label for="name" :value="__('Occupation')" />
-                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name',$occupation->name)" autofocus />
-                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                        <x-input-label for="event" :value="__('Event')" />
+                        <x-text-input id="event" class="block mt-1 w-full" type="text" name="event" :value="old('event',$journey->event)" autofocus />
+                        <x-input-error :messages="$errors->get('event')" class="mt-2" />
                     </div>
                     <div>
-                        <x-input-label for="department" :value="__('Department')" />
-                        <select id="department" name="department" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            @if($page_meta['method'] === 'post')
-                                <option selected>Choose department</option>
-                            @endif
-                            @foreach($departments as $department)
-                                <option value="{{ $department->id }}" @if(isset($occupation->department->id)) {{ $department->id == $occupation->department->id ? 'selected' : '' }} @endif>({{ $department->code }}) {{ $department->name }}</option>
-                            @endforeach
-                        </select>
-                        <x-input-error :messages="$errors->get('department')" class="mt-2" />
+                        <x-input-label for="site" :value="__('Site')" />
+                        <x-text-input id="site" class="block mt-1 w-full" type="text" name="site" :value="old('site',$journey->site)" autofocus />
+                        <x-input-error :messages="$errors->get('site')" class="mt-2" />
+                    </div>
+                    <div>
+                        <x-input-label for="application" :value="__('Application')" />
+                        <x-text-input id="application" class="block mt-1 w-full" type="text" name="application" :value="old('application',$journey->application)" autofocus />
+                        <x-input-error :messages="$errors->get('application')" class="mt-2" />
+                    </div>
+                    <div>
+                        <x-input-label for="origin" :value="__('Origin')" />
+                        <x-text-input id="origin" class="block mt-1 w-full" type="text" name="origin" :value="old('origin',$journey->origin)" autofocus />
+                        <x-input-error :messages="$errors->get('origin')" class="mt-2" />
+                    </div>
+                    <div>
+                        <x-input-label for="destination" :value="__('Destination')" />
+                        <x-text-input id="destination" class="block mt-1 w-full" type="text" name="destination" :value="old('destination',$journey->destination)" autofocus />
+                        <x-input-error :messages="$errors->get('destination')" class="mt-2" />
+                    </div>
+                    <div>
+                        <x-input-label for="date" :value="__('Date')" />
+                        <x-text-input id="date" class="block mt-1 w-full" type="date" name="date" :value="old('date',$journey->date)" autofocus />
+                        <x-input-error :messages="$errors->get('date')" class="mt-2" />
+                    </div>
+                    <div>
+                        <x-input-label for="transportation" :value="__('Transportation')" />
+                        <x-text-input id="transportation" class="block mt-1 w-full" type="text" name="transportation" :value="old('transportation',$journey->transportation)" autofocus />
+                        <x-input-error :messages="$errors->get('transportation')" class="mt-2" />
                     </div>
                     <x-primary-button class="w-32">
-                        {{ __('Create') }}
+                        {{ __('Save') }}
                     </x-primary-button>
-                    <x-danger-button as="a" :href="route('occupations.index')" class="w-32">
+                    <x-danger-button as="a" :href="route('journeys.index')" class="w-32">
                         {{ __('Back') }}
                     </x-danger-button>
                 </form>
