e8bef2d59375ec3e1d861758b07e7eb377191c2e
fajarmori <mrfajar.official@gmail.com>
2024-07-03 16:20:31 +0700

    added UserPolicy in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        Gate::policy(Models\User::class, Policies\UserPolicy::class);
         Gate::policy(Models\Journey::class, Policies\JourneyPolicy::class);
         Gate::policy(Models\Employee::class, Policies\EmployeePolicy::class);
         Gate::policy(Models\Occupation::class, Policies\OccupationPolicy::class);
