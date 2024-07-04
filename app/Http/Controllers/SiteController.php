5d14019fcc86b0666f2ae0ad4cc192f04ca788ce
fajarmori <mrfajar.official@gmail.com>
2024-07-04 14:02:39 +0700

    added code rule in site

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Log;
 use App\Models\Site;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\SiteRequest;
 use App\Http\Controllers\Controller;
@@ -30,7 +31,17 @@ class SiteController extends Controller
 
     public function store(SiteRequest $request)
     {
-        $site = Site::create($request->validated());
+        $user = User::query()->where('email',$request->email)->first();
+        $site = Site::create([
+            'name' => $request->validated('name'),
+            'code' => $request->validated('code'),
+            'owner' => $request->validated('owner'),
+            'district' => $request->validated('district'),
+            'regency' => $request->validated('regency'),
+            'province' => $request->validated('province'),
+            'description' => $request->validated('description'),
+            'user_id' => $user->id??NULL,
+        ]);
         Log::create(['user_id' => auth()->user()->id, 'email' => auth()->user()->email, 'log' => 'created site_id - '.$site->id]);
         return to_route('sites.index');
     }
@@ -55,13 +66,16 @@ class SiteController extends Controller
 
     public function update(SiteRequest $request, Site $site)
     {
+        $user = User::query()->where('email',$request->email)->first();
         $site->update([
-            'name' => $request->name,
-            'owner' => $request->owner,
-            'district' => $request->district,
-            'regency' => $request->regency,
-            'province' => $request->province,
-            'description' => $request->description,
+            'name' => $request->validated('name'),
+            'code' => $request->validated('code'),
+            'owner' => $request->validated('owner'),
+            'district' => $request->validated('district'),
+            'regency' => $request->validated('regency'),
+            'province' => $request->validated('province'),
+            'description' => $request->validated('description'),
+            'user_id' => $user->id??NULL,
         ]);
         Log::create(['user_id' => auth()->user()->id, 'email' => auth()->user()->email, 'log' => 'updated site_id - '.$site->id]);
         return to_route('sites.show',['site' => $site]);
@@ -69,6 +83,7 @@ class SiteController extends Controller
 
     public function destroy(Site $site)
     {
+        $site->update(['code' => 'DEL-'.$site->code,'user_id' => NULL]);
         Site::query()->where('id', $site->id)->delete();
         Log::create(['user_id' => auth()->user()->id, 'email' => auth()->user()->email, 'log' => 'deleted site_id - '.$site->id]);
         return to_route('sites.index');
--- app/Http/Requests/SiteRequest.php
+++ app/Http/Requests/SiteRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class SiteRequest extends FormRequest
@@ -20,6 +21,7 @@ class SiteRequest extends FormRequest
             'regency' => ['required', 'string', 'max:255'],
             'province' => ['required', 'string', 'max:255'],
             'description' => ['required', 'string', 'min:5'],
+            'code' => ['required', 'string', 'min:3', 'max:4', $this->method() == 'POST' ? Rule::unique('sites','code') : Rule::unique('sites','code')->ignore($this->site->id)],
         ];
     }
 }
