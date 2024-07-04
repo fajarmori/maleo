96e6b4b1aa84e425b3e8c402588acae2e5a9e713
fajarmori <mrfajar.official@gmail.com>
2024-07-04 13:57:41 +0700

    added route giat, getemailsite, and move route site

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,10 @@ Route::get('/', Controllers\HomeController::class)->name('home');
 Route::middleware('auth')->group(function () {
     Route::middleware('verified')->group(function () {
         Route::get('/log', Controllers\LogController::class)->name('log');
-        Route::resource('/sites', Controllers\SiteController::class);
     });
     Route::get('/dashboard', Controllers\DashboardController::class)->name('dashboard');
     
+    Route::resource('/sites', Controllers\SiteController::class);
     Route::resource('employees', Controllers\EmployeeController::class)->scoped(['employee' => 'slug']);
     Route::get('journeys/create/{employee:id}', [Controllers\JourneyController::class, 'create'])->name('journeys.create');
     Route::post('journeys/create/{employee:id}', [Controllers\JourneyController::class, 'store'])->name('journeys.store');
@@ -28,7 +28,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [Controllers\ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::view('/gait', 'menu.gait')->name('gait');
+
     Route::get('/getemployees', [Controllers\DataController::class, 'getEmployees'])->name('getemployees');
+    Route::get('/getemailsite', [Controllers\DataController::class, 'getEmailSite'])->name('getemailsite');
 });
 
 require __DIR__.'/auth.php';
