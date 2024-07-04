42c75346959fbf68f206d2be6e336d1f79fb5f0c
fajarmori <mrfajar.official@gmail.com>
2024-07-04 14:03:11 +0700

    added get email site

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Employee;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -11,7 +12,7 @@ class DataController extends Controller
     public function getEmployees(Request $request)
     {
         if($request->ajax()) {
-            $data = Employee::leftJoin('detail_employees', 'employees.id', '=', 'detail_employees.employee_id')
+            $employees = Employee::leftJoin('detail_employees', 'employees.id', '=', 'detail_employees.employee_id')
                 ->where([
                     ['name', 'LIKE', '%'.$request->name.'%'],
                     ['user_id', '=', NULL],
@@ -19,16 +20,41 @@ class DataController extends Controller
                 ])->limit(3)->get();
             $output = '';
 
-            if (count($data)>0) {
-                $output = '<ul class="absolute w-full">';
-                foreach ($data as $row){
-                    $output .= '<li class="bg-white px-3 py-2 border">'.$row->name.' | MRIA-'.substr((10000+$row->mria), -4).'</li>';
+            $output = '<ul class="absolute w-full">';
+            if (count($employees)>0) {
+                foreach ($employees as $employee){
+                    $output .= '<li class="bg-white px-3 py-2 border">'.$employee->name.' | MRIA-'.substr((10000+$employee->mria), -4).'</li>';
                 }
-                $output .= '</ul>';
             }
             else {
-                $output .= '<li class="list-group-item">'.'No results'.'</li>';
+                $output .= '<li class="bg-white px-3 py-2 border">No results</li>';
             }
+            $output .= '</ul>';
+            return $output;
+        }
+    }
+
+    public function getEmailSite(Request $request)
+    {
+        if($request->ajax()) {
+            $users = User::leftJoin('sites', 'users.id', '=', 'sites.user_id')
+                ->where([
+                    ['users.name', 'LIKE', 'site%'],
+                    ['user_id', '=', NULL],
+                    ['email', 'LIKE', '%'.$request->email.'%']
+                ])->limit(3)->get();
+            $output = '';
+
+            $output = '<ul class="absolute w-full">';
+            if (count($users)>0) {
+                foreach ($users as $user){
+                    $output .= '<li class="bg-white px-3 py-2 border">'.$user->email.'</li>';
+                }
+            }
+            else {
+                $output .= '<li class="bg-white px-3 py-2 border">No results</li>';
+            }
+            $output .= '</ul>';
             return $output;
         }
     }
