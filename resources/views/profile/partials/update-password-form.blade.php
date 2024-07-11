56d443ebdf1cc22e1212d23f2c528739310f11fe
fajarmori <mrfajar.official@gmail.com>
2024-07-11 08:19:54 +0700

    add w-32 class in buttom save

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -32,7 +32,7 @@
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <x-primary-button class="w-32">{{ __('Save') }}</x-primary-button>
 
             @if (session('status') === 'password-updated')
                 <p
@@ -40,7 +40,7 @@
                     x-show="show"
                     x-transition
                     x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600"
+                    class="text-sm text-gray-600 w-32"
                 >{{ __('Saved.') }}</p>
             @endif
         </div>
