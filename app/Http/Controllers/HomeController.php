65fa2a4701dddf61a52e51dbb4d87df331f0a977
fajarmori <mrfajar.official@gmail.com>
2024-07-18 13:55:39 +0700

    Final check 02 - OK

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,13 +9,10 @@ use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(Request $request)
     {
         $startDate = Carbon::now()->isoFormat('Y-MM-DD');
-        $endDate = Carbon::now()->addDays(7)->isoFormat('Y-MM-DD');
+        $endDate = Carbon::now()->addDays(14)->isoFormat('Y-MM-DD');
         $countJourney = Journey::all()->whereBetween('date',[ $startDate, $endDate])->count();
         return view('home',compact(['countJourney']));
     }
